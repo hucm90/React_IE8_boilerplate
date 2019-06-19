e13a01efa18b0e5e3c4765525aa2fe8abc183277
Wang <250950393@qq.com>
2019-06-19 17:21:06 +0800

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,39 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const es3ifyPlugin = require('es3ify-webpack-plugin');
 
 const ROOT_PATH = path.resolve(__dirname);
 const SRC_PATH = path.resolve(ROOT_PATH, 'src');
 
 module.exports = {
-    mode: "development",
+    mode: 'development',
 
     // entry: [path.resolve(SRC_PATH, 'index.js')],
     output: {
-        publicPath: "/"
+        publicPath: '/'
     },
 
     resolve: {
-        extensions: ['.js', '.jsx', ".ts", ".tsx"],
-        modules: [SRC_PATH, "node_modules"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        modules: [SRC_PATH, 'node_modules'],
 
         alias: {
-            "@reach/router": "anujs/dist/Router",
-            "react": "anujs/dist/ReactIE.js",
-            "react-dom": "anujs/dist/ReactIE.js",
+            '@reach/router': 'anujs/dist/Router',
+            // "router": "anujs/dist/Router.js",
+            'react': 'anujs/dist/ReactIE.js',
+            'react-dom': 'anujs/dist/ReactIE.js',
             'prop-types': 'anujs/lib/ReactPropTypes',
-            'devtools' : "anujs/lib/devtools",
+            'devtools': 'anujs/lib/devtools',
             'create-react-class': 'anujs/lib/createClass'
         }
     },
 
     module: {
         rules: [
-            // { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader'},
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            { enforce: 'pre', test: /\.(js|jsx|ts|tsx)$/, exclude: /node_modules/, use: 'eslint-loader' },
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
             // { test: /\.(png|jpg|gif|bmp|svg|swf|mp3|ogg)(\?.*$|$)/, loader: "url-loader?limit=2048&name=assets/[hash:5].[ext]"},
             // { test: /\.css$/, use: [{loader: MiniCssExtractPlugin.loader, options: {publicPath: '../'}}, "css-loader" ]},
         ]
@@ -40,10 +41,10 @@ module.exports = {
 
     plugins: [
         new es3ifyPlugin(),
-        new HtmlWebpackPlugin({template: './index.html'}),
+        new HtmlWebpackPlugin({ template: './index.html' }),
         new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
+            filename: '[name].css',
+            chunkFilename: '[id].css'
         })
     ],
 
@@ -52,10 +53,22 @@ module.exports = {
         disableHostCheck: true,
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        open: true
+        open: true,
+        stats: {
+            colors: true,
+            modules: false,
+            children: false,
+            chunks: false,
+            chunkModules: false,
+            chunkOrigins: false,
+            version: false,
+            publicPath: false,
+            assets: false,
+            builtAt: false,
+        }
     },
 
-    devtool: "none"
+    devtool: 'none'
 
     // devtool: 'eval-source-map'
 };
